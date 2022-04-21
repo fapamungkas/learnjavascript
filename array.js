a40f7016664ca9b1486866849e11e4ac9e16129f
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-04-22 05:42:44 +0700

    add/add example array

--- array.js
+++ array.js
@@ -28,3 +28,10 @@ names[2] = "Pamungkas";
 names.push(["Aji", "Muthiah", "Nur", "Azizah"]);
 
 console.table(names);
+
+
+// Menambahkan semua isi dari array
+const angkaArray = [34, 65, 66, 98, 23, 53, 77, 52]
+const totalJumlah = angkaArray.reduce((acc, current) => acc + current, 0)
+
+console.log(`Totalnya adalah ${totalJumlah}`) // output Totalnya adalah 468
