665745a9fc718a49bd628eccea171293811814df
Ferdian Ahmad R <ferdianahmadrozikin10@gmail.com>
2022-04-22 05:43:53 +0700

    add/add example function

--- function.js
+++ function.js
@@ -1,9 +1,18 @@
 //Membuat function
 function sayHelloWorld() {
-  document.writeln("<p>Selamat Istirahat</p>");
+    document.writeln("<p>Selamat Istirahat</p>");
 }
 
 //Memanggil Function
 sayHelloWorld();
 sayHelloWorld();
 sayHelloWorld();
+
+
+//function untuk perkalian 2 angka
+function tambah(angkaPertama, angkaKedua) {
+    const total = angkaPertama * angkaKedua
+    return `Hasil dari ${angkaPertama} * ${angkaKedua} adalah ${total}`
+}
+
+console.log(tambah(35, 50)) //output Hasil dari 35 * 50 adalah 1750
